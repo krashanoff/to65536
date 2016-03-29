1109f37ea0b778f157c3f3bef97b94508baf79d6
Leonid <krashanoff@users.noreply.github.com>
2016-03-29 11:56:03 -0700

    Finally update it so it is correct

--- bundle.js
+++ bundle.js
@@ -1,12 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (Buffer){
+/*
+A cozier, more simple version of bundle.js
+@krashanoff
+*/
+
 var base65536 = require('base65536');
 
 var buffer = new Buffer("hello world");
 
 var convert = base65536.encode(buffer);
 
-console.log(convert);
+document.write(convert);
 
 }).call(this,require("buffer").Buffer)
 },{"base65536":4,"buffer":7}],2:[function(require,module,exports){
