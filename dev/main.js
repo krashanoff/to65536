0cc5495b2c2bb0381721d8e6e35ed85e07e5e826
krashanoff <lkrashanoff2@gmail.com>
2016-03-30 11:24:56 -0700

    Update the code to print out some basic messages

--- dev/main.js
+++ dev/main.js
@@ -3,10 +3,16 @@ A cozier main.min.js (before it gets mangled)
 @krashanoff
 */
 
+//Require
 var base65536 = require('base65536');
 
-var buf = new Buffer("MOTD");
+//Main code
+var motd = "Hello.  This will eventually become a thing where you can convert from base65536 to plaintext and vice versa";
+
+var buf = new Buffer(motd);
 
 var conv = base65536.encode(buf);
 
+//Write to document
 document.write(conv);
+document.write(motd);
